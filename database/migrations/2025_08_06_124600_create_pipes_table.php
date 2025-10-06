bd117ac596a33b4beb6a7742b2dee328b38288fe
davidbelle <davebelle85@gmail.com>
2025-10-06 21:44:26 +1100

    move enabling postgis extension into same migration as pipes

--- database/migrations/2025_08_06_124600_create_pipes_table.php
+++ database/migrations/2025_08_06_124600_create_pipes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Clickbar\Magellan\Schema\MagellanSchema;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,6 +8,8 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        MagellanSchema::enablePostgisIfNotExists($this->connection);
+
         Schema::create('pipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -18,6 +21,9 @@ return new class extends Migration {
 
     public function down(): void
     {
+
         Schema::dropIfExists('pipes');
+
+        MagellanSchema::disablePostgisIfExists($this->connection);
     }
 };
