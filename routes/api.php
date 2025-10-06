cfc6cdd83fb86f7e70b38355ac9912bab3173dff
davidbelle <davebelle85@gmail.com>
2025-10-06 21:58:20 +1100

    remove old route

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,12 @@
 
 use App\Http\Controllers\API\DatapointController;
 use App\Http\Controllers\API\FingerprintController;
-use App\Http\Controllers\API\MapController;
 use App\Http\Controllers\API\MqttAuditController;
 use App\Http\Controllers\API\UserVoiceController;
 use App\Http\Middleware\MfaMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['web','auth'])->group(function () {
-    Route::get('/map/{north}/{east}/{south}/{west}', MapController::class);
-});
+
 
 Route::name('api.')->middleware(['auth:sanctum'])->group(function () {
    Route::resource('datapoints', DatapointController::class);
